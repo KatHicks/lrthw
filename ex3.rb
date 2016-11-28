2a9f13c614965e62e5736472711acdbb6a81c6e9
Kat Hicks <kmhicks92@gmail.com>
2016-11-28 13:50:39 +0000

    Completed exercise 3

---
+++ ex3.rb
@@ -0,0 +1,37 @@
+# prints statement about calculation
+puts "I will now count my chickens:"
+
+# calculates the number of hens and inserts into a string
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# calculates the number of roosters and inserts into a string
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# prints statement about next calculation
+puts "Now I will count the eggs:"
+
+# calculates the number of eggs and prints it
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# prints a question statement
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+# calculates the number of eggs and prints it
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# prints a question statement with the boolean result inserted at the end of the string
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+# prints a question statement with the boolean result inserted at the end of the string
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+
+# prints a statement about previous calculations
+puts "Oh, that's why it's false."
+
+# prints another statement
+puts  "How about some more."
+
+# prints a question statement with the boolean result inserted at the end of the string
+puts "Is it greater? #{5.0 > -2.0}"
+# prints a question statement with the boolean result inserted at the end of the string
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# prints a question statement with the boolean result inserted at the end of the string
+puts "Is it less or equal? #{5.0 <= -2.0}"
---
+++ ex3_studydrill.rb
@@ -0,0 +1,6 @@
+bill = 150
+total_mins = 90
+
+price_permin = bill/total_mins
+
+puts "If the bill came to £#{bill} and it counted a total talk time of #{total_mins} minutes, then the call rate in Lebanon was £#{price_permin} per minute!"
