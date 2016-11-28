639a846aef550159f87622c7590c5ea1ae4b0bf5
Kat Hicks <kmhicks92@gmail.com>
2016-11-28 15:19:53 +0000

    Completed exercise 7

---
+++ ex7.rb
@@ -0,0 +1,41 @@
+# prints a string
+puts "Mary had a little lamb."
+# prints a string with another string interpolated into it
+puts "Its fleece was white as #{'snow'}."
+# prints a string
+puts "And everywhere that Mary went."
+# prints a string ten times
+puts "." * 10 # what'd that do?
+
+# assigns a string variable
+end1 = "C"
+# assigns a string variable
+end2 = "h"
+# assigns a string variable
+end3 = "e"
+# assigns a string variable
+end4 = "e"
+# assigns a string variable
+end5 = "s"
+# assigns a string variable
+end6 = "e"
+# assigns a string variable
+end7 = "B"
+# assigns a string variable
+end8 = "u"
+# assigns a string variable
+end9 = "r"
+# assigns a string variable
+end10 = "g"
+# assigns a string variable
+end11 ="e"
+# assigns a string variable
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+# print does not finish on a new line whereas puts does!
+
+# printing a series of string variables concatenated together
+print end1 + end2 + end3 + end4 + end5 + end6
+# printing a series of string variables concatenated together
+puts end7 + end8 + end9 + end10 + end11 + end12
