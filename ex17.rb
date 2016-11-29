b388780d4745ec2bff5b1ac3cddf92b06356172c
Kat Hicks <kmhicks92@gmail.com>
2016-11-29 17:31:09 +0000

    Completed exercise 17

---
+++ ex17.rb
@@ -0,0 +1,13 @@
+from_file, to_file = ARGV
+
+puts """Copying from #{from_file} to #{to_file}.
+Ready, hit RETURN to continue, CTRL-C to abort.
+"""
+$stdin.gets
+
+from = open(from_file); to = open(to_file, 'w'); to.write(from.read)
+
+puts "Alright, all done."
+
+from.close
+to.close
